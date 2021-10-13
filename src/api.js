04d89e32d3aa60975e7988887c58830ba6689c48
Andrew Cho <andrewgunncho@gmail.com>
2021-10-13 11:11:31 -0700

    mdps done

--- src/api.js
+++ src/api.js
@@ -31,6 +31,18 @@ import ninbg from './images/ninja.png';
 import samicon from './images/samicon.png';
 import sambg from './images/samurai.png';
 
+import blmicon from './images/blmicon.png';
+import blmbg from './images/blackmage.png';
+
+import smnicon from './images/smnicon.png';
+import smnbg from './images/summoner.png';
+
+import rdmicon from './images/rdmicon.png';
+import rdmbg from './images/redmage.png';
+
+import bluicon from './images/bluicon.png';
+import blubg from './images/bluemage.png';
+
 const JobAPI = {
   jobs: [
     { link: 'paladin', bg: pldbg, name: 'PALADIN', icon: palicon, role: 'Tank', description: 'All men seek truth, be they children of Eorzea and freedom, or slaves born into imperial bondage.', embed: '0SQaF6L1rtY'},
@@ -40,10 +52,14 @@ const JobAPI = {
     { link: 'whitemage', bg: whmbg, name: 'WHITE MAGE', icon: whmicon, role: 'Healer', description: 'Even so, in keeping my liberty I have lost much…my journal not least of all.', embed: 'vKboFUS1AzU'},
     { link: 'scholar', bg: schbg, name: 'SCHOLAR', icon: schicon, role: 'Healer', description: 'Twelve be praised, a page of my journal!', embed: 'uLUg_MlnJQg'},
     { link: 'astrologian', bg: astbg, name: 'ASTROLOGIAN', icon: asticon, role: 'Healer', description: 'Verily, the light of truth ever shineth upon they who would bask in its warmth.', embed: 'IHf6r_fuAiE'},
-    { link: 'dragoon', bg: drgbg, name: 'DRAGOON', icon: drgicon, role: 'Melee DPS', description: 'Very well. I shall honor my promise and share with thee that which I know.', embed: 'wmWFZpX8C0Y'},
-    { link: 'monk', bg: mnkbg, name: 'MONK', icon: mnkicon, role: 'Melee DPS', description: 'Each era is ruled by an element, and of these the realm hath borne witness to six.', embed: 'lkK3zTYR7LU'},
-    { link: 'ninja', bg: ninbg, name: 'NINJA', icon: ninicon, role: 'Melee DPS', description: 'Light and darkness yet remain, one of which the Divine Chronicles foretell shall next hold sway.', embed: 'tr4WM76d_ww'},
-    { link: 'samurai', bg: sambg, name: 'SAMURAI', icon: samicon, role: 'Melee DPS', description: 'Ne\'er till land consumes sun can sea bear moons, Heavens spew crimson flame, hells seep black dooms.', embed: 'A9d8eI2ohLA'}
+    { link: 'dragoon', bg: drgbg, name: 'DRAGOON', icon: drgicon, role: 'Melee DPS', description: 'Very well.', embed: 'wmWFZpX8C0Y'},
+    { link: 'monk', bg: mnkbg, name: 'MONK', icon: mnkicon, role: 'Melee DPS', description: 'I shall honor my promise and share with thee that which I know.', embed: 'lkK3zTYR7LU'},
+    { link: 'ninja', bg: ninbg, name: 'NINJA', icon: ninicon, role: 'Melee DPS', description: 'Each era is ruled by an element, and of these the realm hath borne witness to six.', embed: 'tr4WM76d_ww'},
+    { link: 'samurai', bg: sambg, name: 'SAMURAI', icon: samicon, role: 'Melee DPS', description: 'Light and darkness yet remain, one of which the Divine Chronicles foretell shall next hold sway.', embed: 'A9d8eI2ohLA'},
+    { link: 'blackmage', bg: blmbg, name: 'BLACK MAGE', icon: blmicon, role: 'Magic Ranged DPS', description: 'Ne\'er till land consumes sun can sea bear moons, Heavens spew crimson flame, hells seep black dooms.', embed: '9MFGsyddPks'},
+    { link: 'summoner', bg: smnbg, name: 'SUMMONER', icon: smnicon, role: 'Magic Ranged DPS', description: 'Will destruction rain down from the firmament? Or will it seep forth from the bowels of the nether? Whencesoever it cometh, man shall know such devastation as he hath never seen, nay not in his profoundest nightmare.', embed: 'TPbS4L-sLU8'},
+    { link: 'redmage', bg: rdmbg, name: 'RED MAGE', icon: rdmicon, role: 'Magic Ranged DPS', description: 'Whether thou payest the prophecy heed is thy decision to make, and thine alone.', embed: 'iH2nblClyKU'},
+    { link: 'bluemage', bg: blubg, name: 'BLUE MAGE', icon: bluicon, role: 'Magic Ranged DPS', description: 'Yet be fairly warned that disaster waiteth not for man.', embed: '5FAiRM570lY'},
   ],
   all: function() { return this.jobs},
   get: function(link) {
